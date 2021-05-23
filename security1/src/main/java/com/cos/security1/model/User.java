4c903b19fd8feb508624dc25409cad1b16945b37
VitoJeong <ckdghk987@gmail.com>
2021-05-23 23:33:22 +0900

    add : User entity, join/loginPage

---
+++ security1/src/main/java/com/cos/security1/model/User.java
@@ -0,0 +1,31 @@
+package com.cos.security1.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+	@Id @GeneratedValue
+	private Long id;
+	
+	private String username;
+	
+	private String password;
+	
+	private String email;
+	
+	private String role;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+	
+}
